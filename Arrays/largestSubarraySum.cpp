8c72fa2332dd2628c9f653d1d8e9a647a6b19588
AshutoshSingh2021 <85239747+AshutoshSingh2021@users.noreply.github.com>
2022-11-30 21:06:36 +0530

    more array problems

---
+++ Arrays/largestSubarraySum.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+// Print the sum of each subarray & find out the largest sum
+
+void largestSumSubarray(int arr[], int n)
+{
+    int largest_sum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            int subarraySum = 0;
+
+            for (int k = i; k <= j; k++)
+            {
+                subarraySum += arr[k];
+                // if (subarraySum > largest_sum)
+                // {
+                //     largest_sum = subarraySum;
+                // }
+            }
+            largest_sum = max(largest_sum, subarraySum);
+            // subarraySum = 0
+        }
+    }
+    cout << largest_sum << endl;
+}
+
+int main()
+{
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = sizeof(arr) / sizeof(int);
+
+    largestSumSubarray(arr, n);
+
+    return 0;
+}
