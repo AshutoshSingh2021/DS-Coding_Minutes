0f86f897f66db74929dc6f485faa786d4a2ba3bb
AshutoshSingh2021 <85239747+AshutoshSingh2021@users.noreply.github.com>
2022-12-01 23:20:52 +0530

    Prefix sum approach

--- Arrays/largestSubarraySum.cpp
+++ Arrays/largestSubarraySum.cpp
@@ -27,12 +27,40 @@ void largestSumSubarray(int arr[], int n)
     cout << largest_sum << endl;
 }
 
+// Prefix Sum Approach O(N^2)
+int largestSumSubarray1(int arr[], int n)
+{
+
+    int prefix[n] = {0};
+    prefix[0] = arr[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        prefix[i] = prefix[i - 1] + arr[i];
+    }
+
+    int largest_sum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            int subarraySum = i > 0 ? prefix[j] - prefix[i - 1] : prefix[j];
+
+            // check is subarraySum > largest_sum
+            largest_sum = max(largest_sum, subarraySum);
+        }
+    }
+    return largest_sum;
+}
+
 int main()
 {
     int arr[] = {10, 20, 30, 40, 50};
     int n = sizeof(arr) / sizeof(int);
 
     largestSumSubarray(arr, n);
+    cout << largestSumSubarray1(arr, n) << endl;
 
     return 0;
 }
